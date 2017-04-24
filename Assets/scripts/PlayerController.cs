d518d435c714bb4dcb82dec2816eebde326b3687
Arash Badie Modiri <arashbm@gmail.com>
2017-04-24 18:13:17 +0300

    initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+	}
+
+	void FixedUpdate() {
+		float inputForceMultiplier = 5.0f;
+		float horizontalForce = Input.GetAxis ("Horizontal");
+		float verticalForce = Input.GetAxis ("Vertical");
+
+		Vector3 inputForce = (new Vector3(horizontalForce, 0.0f, verticalForce))  * inputForceMultiplier;
+		rb.AddForce(inputForce);
+	}
+}
